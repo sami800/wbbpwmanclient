92ed97d8f50c298364d37fc1ec177aa7adb10bdc
Tom Shedden <thomas.shedden@gmail.com>
2020-02-15 19:26:50 +0000

    Oops I missed off the registration router path
    Added it now though.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserhomeComponent } from './userhome/userhome.component';
 import { LogindialogComponent } from './logindialog/logindialog.component';
 import { PasswordlistComponent } from './passwordlist/passwordlist.component';
+import { RegistrationdialogComponent } from './registrationdialog/registrationdialog.component';
 
 
 const routes: Routes = [
   { path: '', component: UserhomeComponent },
   { path: 'login', component: LogindialogComponent },
   { path: 'home', component: UserhomeComponent },
-  { path: 'passwordlist', component: PasswordlistComponent }
+  { path: 'passwordlist', component: PasswordlistComponent },
+  { path: 'registration', component: RegistrationdialogComponent }
 ];
 
 @NgModule({
