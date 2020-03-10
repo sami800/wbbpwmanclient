d4c3ca96fbc4fbc4a9399fc50d7071a156893709
Tom Shedden <thomas.shedden@gmail.com>
2020-03-10 20:57:50 +0000

    Implemented the Auth Service
    It will always allow login as the gateway is not available. Logging in and out will control what is shown on the page.

--- src/app/logindialog/logindialog.component.ts
+++ src/app/logindialog/logindialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-logindialog',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogindialogComponent implements OnInit {
 
-  constructor() { }
+  username;
+  password;
 
-  ngOnInit(): void {
+  constructor(public auth: AuthService) {}
+
+  ngOnInit(): void {}
+  
+  login(username: string, password: string) {
+    this.auth.login(username, password).subscribe(
+      response => console.log(response),
+      err => console.log(err)
+    );
   }
+  
 
 }
