c65410308d8670f6bb2a5d9935b039333927a096
Tom Shedden <thomas.shedden@gmail.com>
2020-05-01 12:17:50 +0100

    Change to the DB service.

--- src/app/dataservices/local-dbservice.service.ts
+++ src/app/dataservices/local-dbservice.service.ts
@@ -6,14 +6,28 @@ import { IPasswordRecord } from '../models/ipassword-record';
   providedIn: 'root'
 })
 export class LocalDBserviceService extends Dexie {
-  
-  public PWRecords!: Dexie.Table<IPasswordRecord, number>;
+
+  contacts: Dexie.Table<IContactInfo, number>;
+  passwords: Dexie.Table<IPasswordRecord, number>;
 
   constructor() {  
     super("WBBPWManager");
     
-    this.version(1).stores({
-      PWRecords: '++id, domain, password, updatedate',
+    this.version(2).stores({
+      contacts: '++id, first, last',
+      passwords: '++id, domain, password, updatedate',
     });
+    
+    this.contacts = this.table("contacts");
+    this.passwords = this.table("passwords");
+    
+    this.contacts.put({first: "Test", last: "Name"});
+    this.passwords.put({domain: "www.google.com", password: "hashgoeshere", updatedate: "01-01-2020"});
+    this.contacts.put({first: "Test1", last: "Name1"});
+    this.passwords.put({domain: "www.google1.com", password: "hashgoeshere1", updatedate: "01-01-2020"});
+    this.contacts.put({first: "Test2", last: "Name2"});
+    this.passwords.put({domain: "www.google2.com", password: "hashgoeshere2", updatedate: "01-01-2020"});
   }
+  
 }
+
